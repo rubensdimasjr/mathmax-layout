40a6f42713a93d9abc15b03301696c24d6c13d9d
Rubens Júnior <rubens.jr107@gmail.com>
2022-03-06 16:47:27 -0300

    update navbar and section

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,10 +1,26 @@
 import styled from "styled-components";
 
+/* export const Nav = styled.nav`
+  background: #fff;
+  height: 60px;
+  margin-top: -80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`; */
+
 export const BaseButton = styled.button`
   display: flex;
-  /*   justify-self: center; */
+  justify-self: center;
   padding: 5px;
-  margin: 0 5px 0 5px;
+  margin-left: 10px;
   border-radius: 10px;
   border: 0.5px solid #ccc;
   background: transparent;
