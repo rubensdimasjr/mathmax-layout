c6677888e8e82d575986478026ed16bea73af42e
Rubens Júnior <33848110+rubensdimasjr@users.noreply.github.com>
2022-03-06 17:06:05 -0300

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -51,7 +51,7 @@ const Main = () => {
               </Column1>
               <Column2>
                 <ImgWrap>
-                  <Img src="../../images/image1.svg" alt="School" />
+                  <Img src="/src/images/image1.svg" alt="School" />
                 </ImgWrap>
               </Column2>
             </InfoRow>
